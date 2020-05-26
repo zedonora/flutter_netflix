cdcf7f633fd812449918a21276e365e69ca14f66
zedonora <timeundtime@gmail.com>
2020-05-27 05:04:25 +0900

    detail screen add

--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix/model/model_movie.dart';
+import 'package:flutter_netflix/screen/detail_screen.dart';
 
 class CarouselImage extends StatefulWidget {
   final List<Movie> movies;
@@ -97,7 +98,17 @@ class _CarouselImageState extends State<CarouselImage> {
                   padding: EdgeInsets.only(right: 10),
                   child: Column(
                     children: [
-                      IconButton(icon: Icon(Icons.info), onPressed: () {}),
+                      IconButton(
+                          icon: Icon(Icons.info),
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute<Null>(
+                                fullscreenDialog: true,
+                                builder: (BuildContext context) {
+                                  return DetailScreen(
+                                    movie: movies[_currentPage],
+                                  );
+                                }));
+                          }),
                       Text(
                         '정보',
                         style: TextStyle(fontSize: 11),
